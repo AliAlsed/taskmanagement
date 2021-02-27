9344ae19c53ebfba60a5087243fe249b2a30a5e9
AliAlsed <alialsed1996@yahoo.com>
2021-02-27 13:33:05 +0300

    link template to flask app

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
+# url of database //username:passwd@host/database
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/tasks"
 
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    taskname = db.Column(db.String(80), unique=True, nullable=False)
+    member = db.relationship('Employee', uselist=False, backref='task')
+    duedate = db.Column(db.String(120),nullable=False)
+
+class Employee(db.Model):
+    __tablename__ = 'employee'
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
+    taskname = db.Column(db.String(80), unique=True, nullable=False)
+
+db.create_all()
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('first.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,port=3000)
