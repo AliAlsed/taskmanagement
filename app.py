04b5757e6fcf05c11aa6bcb5e4e2e81b6ef0f233
AliAlsed <alialsed1996@yahoo.com>
2021-03-02 21:18:03 +0300

    add task , show

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,22 +11,37 @@ class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
     taskname = db.Column(db.String(80), unique=True, nullable=False)
-    member = db.relationship('Employee', uselist=False, backref='task')
-    duedate = db.Column(db.String(120),nullable=False)
+    leader =  db.Column(db.String(80), nullable=False)
+    # status = db.relationship('Employee', uselist=False, backref='task')
+    status = db.Column(db.String(80),  nullable=False)
+    event = db.Column(db.String(80),  nullable=False)
+    # duedate = db.Column(db.String(120),nullable=False)
 
-class Employee(db.Model):
-    __tablename__ = 'employee'
-    id = db.Column(db.Integer, primary_key=True)
-    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
-    taskname = db.Column(db.String(80), unique=True, nullable=False)
+# class Employee(db.Model):
+#     __tablename__ = 'employee'
+#     id = db.Column(db.Integer, primary_key=True)
+#     task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
+#     taskname = db.Column(db.String(80), unique=True, nullable=False)
 
 db.create_all()
 @app.route('/')
 def hello_world():
     return render_template('first.html')
-@app.route('/admin')
+@app.route('/admin', methods=['GET','POST'])
 def admin():
-    return render_template('dash.html')
+    if request.method == 'POST':
+        task = request.form['task']
+        leader = request.form['leader']
+        status = request.form['status']
+        event = request.form['event']
+        task = Task(taskname=task,leader=leader,status=status,event=event)
+        db.session.add(task)
+        db.session.commit()
+    tasks = Task.query.all()
+    if tasks != None:
+        return render_template('dash.html',tasklist = tasks)
+    else:
+        return render_template('dash.html')
 @app.route('/admin/signin')
 def login():
     return render_template('dashboard/login.html')
